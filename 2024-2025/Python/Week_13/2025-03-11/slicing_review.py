574255246f5852876534a3bef2a58e1a7b3c720c
Tayden Vicente <tvicente@bostonk12.org>
2025-03-11 10:48:58 -0400

    Started classwork for 3/11/2025.

--- 2024-2025/Python/Week_13/2025-03-11/slicing_review.py
+++ 2024-2025/Python/Week_13/2025-03-11/slicing_review.py
@@ -1,2 +1,6 @@
 #Tayden Vicente
 # March 11, 2025
+
+first_name=input("what is your first name?")
+Last_name=input("what is your last name?")
+print("Look what i did to your name!",first_name [::-1],Last_name[::-1])
